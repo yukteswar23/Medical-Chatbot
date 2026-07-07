2f51f5d5936509338d948940481fd6c7dea8f145
ymantha23 <pythoneswar23@gmail.com>
2026-07-07 22:37:48 +0530

    folder structure has been created

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating folders
+mkdir -p src
+mkdir -p research
+
+# Creating files 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch req.txt
+
+echo "Files created successfully"
+
+
