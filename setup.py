ad701085d15509732b1d6d10b8b5c7b627a9da15
ymantha23 <pythoneswar23@gmail.com>
2026-07-09 10:04:40 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name='medical_chatbot' ,
+    version='0.1.0' , 
+    author='Yukteswar Mantha' , 
+    author_email='ymantha23@gmail.com' , 
+    packages = find_packages() , 
+    install_requires=[] 
+)
